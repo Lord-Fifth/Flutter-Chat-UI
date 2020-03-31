20cd2fcc423983e6199cc442d8cafc7c0eda1f47
Lord-Fifth <aditya.suresh098@gmail.com>
2020-03-31 22:32:30 +0530

    Rudimentary Messages UI

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui_starter/models/message_model.dart';
 import 'package:flutter_chat_ui_starter/models/user_model.dart';
 
 class ChatScreen extends StatefulWidget {
 
   final User user;
-
   ChatScreen({this.user});
 
   @override
@@ -14,6 +14,55 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        title: Text(
+          widget.user.name,
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ],
+      ),
+      
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    topLeft: Radius.circular(30.0),
+                ),
+              ),
+
+              child: ClipRect(
+                child: ListView.builder(
+                  padding: EdgeInsets.only(top: 15.0),
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      final Message message = messages[index];
+                      return Text(
+                          message.text
+                      );
+                    },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
